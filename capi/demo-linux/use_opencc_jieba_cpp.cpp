1e018e1f31e1ab16550b5cadde67fb2b9a247ddd
laisuk <laisuk@yahoo.com>
2024-11-02 02:57:42 +0800

    Version 0.2.0 Update Linux .so

--- capi/demo-linux/use_opencc_jieba_cpp.cpp
+++ capi/demo-linux/use_opencc_jieba_cpp.cpp
@@ -2,21 +2,21 @@
 #include "opencc_jieba_capi.h"
 
 int main(int argc, char **argv) {
-    auto opencc = opencc_new();
+    auto opencc = opencc_jieba_new();
     const char *config = u8"s2twp";
     const char *text = u8"意大利罗浮宫里收藏的“蒙娜丽莎的微笑”画像是旷世之作。";
     std::cout << "Text: " << text << "\n";
-    auto code = opencc_zho_check(opencc, text);
+    auto code = opencc_jieba_zho_check(opencc, text);
     std::cout << "Text Code: " << code << "\n";
-    char *result = opencc_convert(opencc, text, config, true);
-    code = opencc_zho_check(opencc, result);
+    char *result = opencc_jieba_convert(opencc, text, config, true);
+    code = opencc_jieba_zho_check(opencc, result);
     std::cout << "Converted: " << result << "\n";
     std::cout << "Text Code: " << code << "\n";
     if (result != NULL) {
-        opencc_string_free(result);
+        opencc_jieba_free_string(result);
     }
     if (opencc != NULL) {
-        opencc_free(opencc);
+        opencc_jieba_free(opencc);
     }
 
     return 0;
--- capi/demo-linux/use_opencc_jieba_cut.cpp
+++ capi/demo-linux/use_opencc_jieba_cut.cpp
@@ -3,21 +3,21 @@
 
 int main() {
     // Create OpenCC instance
-    void *instance = opencc_new();
+    void *instance = opencc_jieba_new();
     // Input string
     const char *input = "你好，世界！";
     // Delimiter
     const char *delimiter = "/ ";
     // Perform segmentation and join with delimiter
-    char *result = opencc_jieba_cut_and_join(instance, input, true, delimiter);
+    char *result = opencc_jieba_cut_and_join(instance, input, false, delimiter);
     if (result != NULL) {
         // Print the result
         std::cout << "Result: " << result << std::endl;
         // Free memory
-        opencc_string_free(result);
+        opencc_jieba_free_string(result);
     }
     // Free OpenCC instance
-    opencc_free(instance);
+    opencc_jieba_free(instance);
 
     return 0;
 }
