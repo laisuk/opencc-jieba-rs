c53d0dfebdd616281b9c08df089b84b3821ebe86
laisuk <laisuk@yahoo.com>
2026-01-11 13:10:45 +0800

    Introduce Enum OpenccConfig

--- capi/demo-win/opencc_jieba_capi.h
+++ capi/demo-win/opencc_jieba_capi.h
@@ -18,7 +18,7 @@ extern "C" {
  *
  * @return A pointer to a new instance of OpenCC JIEBA.
  */
-void *opencc_jieba_new();
+void *opencc_jieba_new(void);
 
 /**
  * Converts a null-terminated UTF-8 input string using the specified OpenCC config.
@@ -52,7 +52,7 @@ int opencc_jieba_zho_check(const void *instance, const char *input);
  * @param instance A pointer to an instance previously returned by `opencc_jieba_new`.
  *                 Passing NULL is safe and does nothing.
  */
-void opencc_jieba_delete(const void *instance);
+void opencc_jieba_delete(void *instance);
 
 /**
  * @deprecated Use `opencc_jieba_delete()` instead.
@@ -62,7 +62,7 @@ void opencc_jieba_delete(const void *instance);
  * @param instance A pointer to an instance previously returned by `opencc_jieba_new`.
  *                 Passing NULL is safe and does nothing.
  */
-void opencc_jieba_free(const void *instance);
+void opencc_jieba_free(void *instance);
 
 /**
  * Frees a string returned by `opencc_jieba_convert`, `opencc_jieba_cut_and_join`,
@@ -71,7 +71,7 @@ void opencc_jieba_free(const void *instance);
  * @param ptr A pointer to a string previously returned by the API.
  *            Passing NULL is safe and does nothing.
  */
-void opencc_jieba_free_string(const char *ptr);
+void opencc_jieba_free_string(char *ptr);
 
 /**
  * Performs segmentation on a UTF-8 input string using Jieba.
@@ -103,7 +103,7 @@ void opencc_jieba_free_string_array(char **array);
  * @return A newly allocated string with all parts joined by the delimiter.
  *         Must be freed using `opencc_jieba_free_string()`.
  */
-char *opencc_jieba_join_str(char **strings, const char *delimiter);
+char *opencc_jieba_join_str(char *const *strings, const char *delimiter);
 
 /**
  * Segments and joins an input string using Jieba, with the specified delimiter.
@@ -129,7 +129,7 @@ char *opencc_jieba_cut_and_join(const void *instance, const char *input, bool hm
  * @return A NULL-terminated array of UTF-8 C strings representing keywords.
  *         Must be freed using `opencc_jieba_free_string_array()`.
  */
-char **opencc_jieba_keywords(const void *instance, const char *input, int top_k, const char *method);
+char **opencc_jieba_keywords(const void *instance, const char *input, size_t top_k, const char *method);
 
 /**
  * Extracts keywords and their corresponding weights using TextRank or TF-IDF.
