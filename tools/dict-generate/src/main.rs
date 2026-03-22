f6087c67c7006428bf1c9e20f7e6e44c766927c8
laisuk <laisuk@yahoo.com>
2026-03-23 06:03:47 +0800

    Fixed ureq DendencyBot error, Removed

--- tools/dict-generate/src/main.rs
+++ tools/dict-generate/src/main.rs
@@ -1,11 +1,8 @@
-use clap::{Arg, ArgAction, Command};
+use clap::{Arg, Command};
 use opencc_jieba_rs::dictionary_lib::Dictionary;
 use std::fs::File;
-use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
-use std::time::Duration;
-use std::{env, fs, io};
-use ureq::Agent;
+use std::{env, io};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     const BLUE: &str = "\x1B[1;34m";
@@ -27,12 +24,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .value_name("filename")
                 .help("Write generated dictionary to <filename>. If not specified, a default filename is used."),
         )
-        .arg(
-            Arg::new("download_dicts")
-                .long("download-dicts")
-                .action(ArgAction::SetTrue)
-                .help("Download missing OpenCC dict files into ./dicts/ from GitHub (non-interactive)."),
-        )
         .about(format!(
             "{BLUE}Dict Generator: Command Line Dictionary Generator for opencc-jieba-rs{RESET}"
         ))
@@ -40,21 +31,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let dict_dir = Path::new("dicts");
     if !dict_dir.exists() {
-        let want_download = matches.get_flag("download_dicts");
-
-        if !want_download {
-            eprintln!(
-                "{BLUE}Local 'dicts/' not found.{RESET}\n\
-                 {BLUE}Hint:{RESET} run with {BLUE}--download-dicts{RESET} to fetch from GitHub, \
-                 or provide the dict files manually.\n\
-                 {BLUE}Exiting.{RESET}"
-            );
-            std::process::exit(2);
-        }
-
-        eprintln!("{BLUE}Local 'dicts/' not found. Downloading from GitHub...{RESET}");
-        fetch_dicts_from_github(dict_dir)?;
-        eprintln!("{BLUE}Tip: reuse ./dicts/ for future runs to skip downloads.");
+        eprintln!(
+            "{BLUE}Error:{RESET} required directory {BLUE}./dicts/{RESET} not found.\n\
+     Please provide OpenCC dictionary files in the {BLUE}dicts/{RESET} folder.\n\
+     See: https://github.com/laisuk/opencc-jieba-rs/tree/master/dicts\n\
+     {BLUE}Exiting.{RESET}"
+        );
+        std::process::exit(2);
     }
 
     let dict_format = matches.get_one::<String>("format").map(String::as_str);
@@ -107,54 +90,4 @@ fn to_abs_path(p: impl AsRef<Path>) -> io::Result<PathBuf> {
     } else {
         Ok(env::current_dir()?.join(p))
     }
-}
-
-/// Download missing dict files from GitHub repo
-fn fetch_dicts_from_github(dict_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let dict_files = [
-        "STCharacters.txt",
-        "STPhrases.txt",
-        "TSCharacters.txt",
-        "TSPhrases.txt",
-        "TWPhrases.txt",
-        "TWPhrasesRev.txt",
-        "TWVariants.txt",
-        "TWVariantsRev.txt",
-        "TWVariantsRevPhrases.txt",
-        "HKVariants.txt",
-        "HKVariantsRev.txt",
-        "HKVariantsRevPhrases.txt",
-        "JPShinjitaiCharacters.txt",
-        "JPShinjitaiPhrases.txt",
-        "JPVariants.txt",
-        "JPVariantsRev.txt",
-    ];
-
-    fs::create_dir_all(dict_dir)?;
-
-    let config = Agent::config_builder()
-        .timeout_global(Some(Duration::from_secs(10)))
-        .build();
-    let agent: Agent = config.into();
-
-    for filename in &dict_files {
-        let url = format!(
-            "https://raw.githubusercontent.com/laisuk/opencc-jieba-rs/master/dicts/{}",
-            filename
-        );
-
-        let response = agent.get(&url).call()?;
-
-        // Read as bytes (don’t assume UTF-8)
-        let mut bytes = Vec::new();
-        response.into_body().into_reader().read_to_end(&mut bytes)?;
-
-        let dest_path = dict_dir.join(filename);
-        let mut file = File::create(dest_path)?;
-        file.write_all(&bytes)?;
-
-        eprintln!("Downloaded: {}", filename);
-    }
-
-    Ok(())
 }
